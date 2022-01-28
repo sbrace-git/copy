f008f0b36fc5c9054abff4a4d7906b3a585d8b07
sbrace <sbrace0822@gmail.com>
2022-01-28 11:45:57 +0800

    add log

--- src/main/java/Copy.java
+++ src/main/java/Copy.java
@@ -5,7 +5,7 @@ import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
+import java.util.*;
 
 public class Copy {
 
@@ -16,26 +16,30 @@ public class Copy {
             return;
         }
 
-        Path inputPath = Paths.get(args[0]).toAbsolutePath();
+        Path inputPath = Paths.get(args[0]);
 
         if (!Files.exists(inputPath)) {
             System.out.println("input path is not exists");
             return;
         }
 
-        Path outputFilePath = inputPath.getParent().resolve(
-                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss-SSS-")) + inputPath.getFileName());
+        Path outputFilePath = inputPath.getParent().resolve(LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss-SSS-")) + inputPath.getFileName());
 
         if (Files.isDirectory(inputPath)) {
             final String inputAbsolutePath = inputPath.toAbsolutePath().toString();
             final String outputAbsolutePath = outputFilePath.toAbsolutePath().toString();
+
+            final List<Path> visitFileFailedList = new LinkedList<>();
+            final List<Path> createDirectoryList = new LinkedList<>();
+            final List<Path> createFileList = new LinkedList<>();
 
             Files.walkFileTree(inputPath, new SimpleFileVisitor<Path>() {
                 @Override
                 public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
                     Path directoryPath = Paths.get(dir.toString().replace(inputAbsolutePath, outputAbsolutePath));
                     Files.createDirectories(directoryPath);
-                    System.out.println("create directory : " + directoryPath.toAbsolutePath());
+                    createDirectoryList.add(directoryPath);
                     return FileVisitResult.CONTINUE;
                 }
 
@@ -43,16 +47,25 @@ public class Copy {
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                     Path filePath = Paths.get(file.toString().replace(inputAbsolutePath, outputAbsolutePath));
                     Files.copy(file, filePath);
-                    System.out.println("create file : " + filePath.toAbsolutePath());
+                    createFileList.add(filePath);
                     return FileVisitResult.CONTINUE;
                 }
 
                 @Override
                 public FileVisitResult visitFileFailed(Path file, IOException exc) {
-                    System.out.println("visitFileFailed file = " + file);
+                    visitFileFailedList.add(file);
                     return FileVisitResult.CONTINUE;
                 }
             });
+
+            System.out.println("create directory count = " + createDirectoryList.size());
+            createDirectoryList.forEach(System.out::println);
+
+            System.out.println("create file count = " + createFileList.size());
+            createFileList.forEach(System.out::println);
+
+            System.out.println("visit file failed count = " + visitFileFailedList.size());
+            visitFileFailedList.forEach(System.out::println);
         } else {
             Files.copy(inputPath, outputFilePath);
             System.out.println("create file : " + outputFilePath.toAbsolutePath());
